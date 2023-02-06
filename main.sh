0063b58b7992c18850418d2c99fcdfda341e7333
jihooni0914 <dbwlgns8314@gmail.com>
2023-02-07 02:02:50 +0900

    add user password

--- main.sh
+++ main.sh
@@ -7,26 +7,27 @@ echo "$repo_name"
 
 git clone $repo_url
 
-chmod +x make_package_dot_json.sh make_src_directory.sh make_appserver.sh make_config.sh make_routes.sh
+# chmod +x make_package_dot_json.sh make_src_directory.sh make_appserver.sh make_config.sh make_routes.sh
 cd $repo_name
-../make_package_dot_json.sh $repo_name $repo_url
-../make_src_directory.sh
-../make_appserver.sh
-../make_config.sh
-../make_routes.sh
-ls
+# ../make_package_dot_json.sh $repo_name $repo_url
+# ../make_src_directory.sh
+# ../make_appserver.sh
+# ../make_config.sh
+# ../make_routes.sh
+# ls
 
 # npm install
 
 git config --global user.name $2
 git config --global user.name 
-git config --global user.email $3@gmail.com
+git config --global user.email $2@gmail.com
 git config --global user.email
+git config --global user.password $3
 
 # git remote set-url origin git@github.com:$2/$repo_name.git
 
 git add .
 git commit -m "init"
-# git push
+git push
 # git push -u origin main
 git push https://$2:$3@github.com/$2/$repo_name.git
